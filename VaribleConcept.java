a4d909c07b3e9773328ad7e8fa2f20e580b466df
Nivetha2204 <nivethasathiamoorthy22@gmail.com>
2024-06-27 22:43:08 +0530

    Intial commit

---
+++ VaribleConcept.java
@@ -0,0 +1,20 @@
+
+public class VaribleConcept{
+
+
+	String studentName = "Ram";
+
+	static String collegeName = "BesantTech";
+
+	public static void main(String args[]){
+
+		int studentid = 56789;
+		System.out.println(studentid);
+		System.out.println(collegeName);
+		VaribleConcept vc = new VaribleConcept();
+		System.out.println(vc.studentName);
+
+	}
+
+
+}
